9e63877bc7d53f3bc4cab04802f9197f7138c52c
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-07-18 20:01:51 +0530

    first commit

---
+++ labs/FirstApplication/FirstApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FirstApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number1, Number2;
+            Console.WriteLine("Please enter some number 1");
+            Number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter some number 2");
+            Number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(Number1+"\t");
+            Console.Write(Number2);
+
+            if ()
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
