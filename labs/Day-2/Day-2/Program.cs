0db1bc0ab261a7f3b0266194dc2e973d50b0b351
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-07-20 09:52:45 +0530

    added typecasting

---
+++ labs/Day-2/Day-2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Day_2
+{
+    internal class Program
+    {
+        // Implicit vs explicit
+
+        /*
+         * 
+         */
+        static void Main1(string[] args)
+        {
+            int Number = 15;
+            //get type
+            Type typeOfVariable=Number.GetType();
+
+            //Implicit
+            double numDouble = Number;
+            Console.WriteLine(numDouble);// increment
+
+            int NumerInt = (int)numDouble;//decrement
+
+            int NumerLong = Convert.ToInt32(numDouble);
+        }
+    }
+}
