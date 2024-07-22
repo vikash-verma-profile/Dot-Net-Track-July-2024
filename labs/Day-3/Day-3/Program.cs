57d26e5af142c6f6980ae03702c8c20ae2b39bb6
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-07-22 19:16:24 +0530

    Added code

---
+++ labs/Day-3/Day-3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Day_3
+{
+    internal class Program
+    {
+        public static void swap(int a,int b)
+        {
+            int c = a;
+            a= b;
+            b = c;      
+            Console.WriteLine("Value of a and b is a= "+a+"b= "+b);
+        }
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2;
+            Console.WriteLine("Value of numbers a= "+a+"b= "+b);
+            swap(a, b);
+            Console.WriteLine("Value of numbers after swap a= " + a + "b= " + b);
+        }
+    }
+}
