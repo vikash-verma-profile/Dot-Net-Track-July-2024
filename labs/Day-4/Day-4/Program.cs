862d76bc76906dcaaea7544d268927a6a6f33e33
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-07-25 19:14:54 +0530

    added cpode

---
+++ labs/Day-4/Day-4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Day_4
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            string Number = "12";
+            //int ParsedNumber=int.Parse(Number);
+            bool Isint  = int.TryParse(Number,out int ParsedNumber);
+            if (Isint)
+            {
+                Console.WriteLine("You parse NUmber is : " + ParsedNumber);
+            }
+            else
+            {
+                Console.WriteLine("its not a number");
+            }
+          
+
+        }
+    }
+}
---
+++ labs/Day-4/Day-4/StringvsStringBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4
+{
+    internal class StringvsStringBuilder
+    {
+        static void Main(string[] args)
+        {
+            string s = string.Empty;
+            s = "Vikash";
+            s.ToUpper();
+            StringBuilder sb=new StringBuilder();
+            sb.Append(s);
+            sb.Clear();
+            Console.WriteLine(sb.ToString());
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine(s[i]);
+            }
+        }
+    }
+}
