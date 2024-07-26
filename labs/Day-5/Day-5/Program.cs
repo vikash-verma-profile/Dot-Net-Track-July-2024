325a51d370063e07852c78cd81223d44d0396c98
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-07-26 18:46:20 +0530

    Added constructors

---
+++ labs/Day-5/Day-5/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Day_5
+{
+
+    class Student
+    {
+        public int Id;
+        public string StudentName;
+        public string StudentEmail;
+
+        public Student(int id,string Name)
+        {
+            Id= id;
+            StudentName= Name;
+        }
+        public void PrintStudentData()
+        {
+            Console.Write(StudentName);
+            Console.Write(Id);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student(1,"Vikash");
+            student.PrintStudentData();
+        }
+    }
+}
