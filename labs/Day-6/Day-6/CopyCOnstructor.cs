f58850db671f84a840613a18af22af531bf6aa8b
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-07-27 09:28:02 +0530

    added code

---
+++ labs/Day-6/Day-6/CopyCOnstructor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_6
+{
+    class Vehicle
+    {
+        private string name;
+        private string color;
+        //copy constructor
+        public Vehicle(Vehicle vehicle)
+        {
+            name = vehicle.name;
+            color = vehicle.color;
+        }
+        public Vehicle(string name,string color)
+        {
+            this.name = name;
+            this.color = color;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Model Name:" + name+"\n color: "+color);
+        }
+    }
+    internal class CopyCOnstructor
+    {
+        public static void Main()
+        {
+            Vehicle v1 = new Vehicle("Bike","Black");
+            Vehicle v2 = new Vehicle(v1);
+            v2.PrintData();
+
+
+        }
+    }
+}
---
+++ labs/Day-6/Day-6/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Day_6
+{
+
+    class Demo
+    {
+        //default
+        //public Demo()
+        //{
+            
+        //}
+        static Demo()
+        {
+
+        }
+        private Demo()
+        {
+
+        }
+        public Demo(int a)
+        {
+            Console.WriteLine(a);
+        }
+    }
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+           // Demo d = new Demo();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
