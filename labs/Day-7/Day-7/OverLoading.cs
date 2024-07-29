1b761a3f0d25667a4c27d6786811a012cc868330
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-07-29 18:54:23 +0530

    Added code

---
+++ labs/Day-7/Day-7/OverLoading.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_7
+{
+
+    public class OverloadingDemo
+    {
+        public void Sum()
+        {
+            Console.WriteLine(1 + 2);
+        }
+
+        public void Sum(int a,int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void Sum(string a, string b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void Sum(int a, int b,int c)
+        {
+            Console.WriteLine(a + b+c);
+        }
+    }
+    internal class OverLoading
+    {
+        public static void Main()
+        {
+            OverloadingDemo overloadingDemo = new OverloadingDemo();
+            overloadingDemo.Sum();
+            overloadingDemo.Sum(1,2);
+            overloadingDemo.Sum(1, 2,3);
+            overloadingDemo.Sum("1","2");
+
+        }
+    }
+}
---
+++ labs/Day-7/Day-7/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Day_7
+{
+
+    public abstract class Animal
+    {
+        public abstract string Sound { get; }
+    }
+    public class Cat : Animal
+    {
+        public override string Sound => "some sound";
+
+    }
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
