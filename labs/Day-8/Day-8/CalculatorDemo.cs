7850ebd856bcd18b1757ada02b72e52221b47182
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-08-01 20:26:05 +0530

    Added code

--- labs/Day-8/Day-8/CalculatorDemo.cs
+++ labs/Day-8/Day-8/CalculatorDemo.cs
@@ -7,8 +7,14 @@ using Calculator;
 
 namespace Day_8
 {
+
+    public class demo1
+    {
+
+    }
     internal class CalculatorDemo
     {
+        public demo1 demo { get; set; }
         public static void Main()
         {
             CalculatorClient obj=new CalculatorClient();
