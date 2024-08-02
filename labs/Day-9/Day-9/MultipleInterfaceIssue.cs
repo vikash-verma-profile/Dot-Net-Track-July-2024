acf32ef2003339b7e842db88b4ca458b0ae169ab
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-08-02 19:02:04 +0530

    Added interface

---
+++ labs/Day-9/Day-9/MultipleInterfaceIssue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_9
+{
+
+    interface IWholeSeller
+    {
+        void Discount();
+    }
+    interface IRetailer
+    {
+         void Discount();
+    }
+    class Buyer : IWholeSeller, IRetailer
+    {
+         void IWholeSeller.Discount() {
+            Console.WriteLine("Hi i am called from DEMO IWholeSeller");
+        }
+         void IRetailer.Discount()
+        {
+            Console.WriteLine("Hi i am called from DEMO IRetailer");
+        }
+        public void Print()
+        {
+            
+        }
+    }
+    internal class MultipleInterfaceIssue
+    {
+        public static void Main()
+        {
+            IWholeSeller d1 = new Buyer();
+            d1.Discount();
+
+            IRetailer d2 = new Buyer();
+            d2.Discount();
+
+        }
+    }
+}
---
+++ labs/Day-9/Day-9/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Day_9
+{
+    interface ITest
+    {
+        void TestMethod();
+    }
+    class TestClass : ITest
+    {
+        public void TestMethod() {
+
+            Console.WriteLine("Implicit Interface Implementation");
+        }
+    }
+    class ExplicitTestClass : ITest {
+
+        void ITest.TestMethod()
+        {
+            Console.WriteLine("Explicit Interface Implementation");
+        }
+    }
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
