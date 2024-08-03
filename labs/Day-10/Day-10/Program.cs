0bc0952225f14cf7aea8dce3c3b84acc0eb7b724
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-08-03 09:26:54 +0530

    Added code

---
+++ labs/Day-10/Day-10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace Day_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            string str = "Vikash Verma";
+            int x = 7;
+            DateTime d =DateTime.Parse("03-08-2024");
+
+            al.Add(str);
+            al.Add(x);
+            al.Add(d);
+
+            foreach (var item in al)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("===============================");
+            Hashtable ht=new Hashtable();
+            ht.Add("ora", "Oracle");
+            ht.Add("vb", "vb.net");
+            ht.Add("cs", "cs.net");
+            ht.Add("asp", "asp.net");
+
+            foreach (DictionaryEntry item in ht)
+            {
+                Console.WriteLine(item.Key+" "+item.Value);
+            }
+
+        }
+    }
+}
