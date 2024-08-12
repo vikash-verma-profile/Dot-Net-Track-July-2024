bdcd73b100fb11e7f95d81c97bed07a0dd0e837a
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-08-12 18:48:45 +0530

    added csv writer

---
+++ labs/Day-14/Day-14/Program.cs
@@ -0,0 +1,35 @@
+﻿using CsvHelper;
+using System.Globalization;
+using CsvHelper.Configuration;
+
+namespace Day_14
+{
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var directory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent;
+            var filePath = directory + "\\csvfiles\\books.csv";
+
+            List<Person> peopleList = new List<Person>
+            {
+                new Person{FirstName="Vikash",LastName="Verma",Age=40},
+                new Person{FirstName="Satish",LastName="Sharma",Age=70}
+            };
+
+            using var writer = new StreamWriter(filePath);
+            using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
+            {
+                csv.WriteRecords(peopleList);
+            }
+
+        }
+    }
+}
