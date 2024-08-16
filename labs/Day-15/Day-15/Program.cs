dac6ca5b84561cffa68d6d59fa15426a08ae59bb
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-08-16 18:58:00 +0530

    Added code

---
+++ labs/Day-15/Day-15/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+
+namespace Day_15
+{
+    public class Employee
+    {
+        public string EmpID { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+
+        public static void WriteData(string fileName,string jsonString)
+        {
+            File.WriteAllText(fileName, jsonString);
+        }
+
+        public static Employee ReadData(string fileName)
+        {
+           var jsonString= File.ReadAllText(fileName);
+            var employee = JsonSerializer.Deserialize<Employee>(jsonString);
+            return employee;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee()
+            {
+                EmpID = "EMP123",
+                ID = 123,
+                Gender = "Male",
+                Name = "Vikash Verma",
+                Salary = 9090
+            };
+
+            string jsonString=JsonSerializer.Serialize(employee);
+            string fileName = "employee.json";
+
+            Employee.WriteData(fileName, jsonString);
+            var EmployeeData= Employee.ReadData(fileName);
+            Console.WriteLine("Employee Data: "+EmployeeData.Name);
+
+
+            
+        }
+    }
+}
