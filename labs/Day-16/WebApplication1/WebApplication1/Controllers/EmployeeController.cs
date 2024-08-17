6b00af801c107f61cea60d498ca3ee074e4a78e1
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-08-17 10:40:18 +0530

    Added datta

---
+++ labs/Day-16/WebApplication1/WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.ViewModel;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public Employee GetEmployee()
+        {
+            return new Employee { Id=123,Name="Vikash Verma",Gender="Male",Salary=1000};
+        }
+    }
+}
---
+++ labs/Day-16/WebApplication1/WebApplication1/ViewModel/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.ViewModel
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+    }
+}
