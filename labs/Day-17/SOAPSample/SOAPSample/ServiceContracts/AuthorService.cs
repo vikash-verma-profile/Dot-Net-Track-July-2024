1246b2437ec972b28c6d72f393083f4d61803527
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-08-19 19:12:54 +0530

    added soap sample

---
+++ labs/Day-17/SOAPSample/SOAPSample/ServiceContracts/AuthorService.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceModel;
+using System.Xml.Linq;
+
+namespace SOAPSample.ServiceContracts
+{
+    [ServiceContract]
+    public interface IAuthorService
+    {
+        [OperationContract]
+        void MySoapMethod(XElement xml);
+    }
+    public class AuthorService : IAuthorService
+    {
+        public void MySoapMethod(XElement xml)
+        {
+            Console.WriteLine(xml.ToString());
+        }
+    }
+}
