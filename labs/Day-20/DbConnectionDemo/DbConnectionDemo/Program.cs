2aeb2009d6278fd4a84590bffb2b59b8116cbdc4
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-08-24 09:46:38 +0530

    added dbconnection

---
+++ labs/Day-20/DbConnectionDemo/DbConnectionDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+
+namespace DbConnectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new 
+                SqlConnection("Data Source=DESKTOP-HVUT504;Initial Catalog=EmployeeDB;Integrated Security=True;");
+            SqlCommand cmd = new SqlCommand("select * from tblEmployee",con);
+            con.Open();
+            SqlDataReader rdr=cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr["id"]+" | " + rdr["EmployeeName"]);
+            }
+        }
+    }
+}
