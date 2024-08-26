bda7893c52454517f94d83d6e0d99dc246548c28
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-08-26 18:55:11 +0530

    Added code

---
+++ labs/EFDemoCodeFirst/EFDemoCodeFirst/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFDemoCodeFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ labs/EFDemoCodeFirst/EFDemoCodeFirst/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace EFDemoCodeFirst.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
