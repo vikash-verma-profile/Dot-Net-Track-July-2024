8d42d07449a0792dd0214cf2e90557b5e6e8616e
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-08-30 19:06:14 +0530

    added code

---
+++ labs/Day-21/SeleniumTest/SeleniumTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+        }
+    }
+}
