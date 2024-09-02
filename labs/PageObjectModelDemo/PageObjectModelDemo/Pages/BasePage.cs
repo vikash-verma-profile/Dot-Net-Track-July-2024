80cc1b8ac961a4cfd2ee4aa5309db86ce441a9ec
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-09-02 18:57:33 +0530

    added code

---
+++ labs/PageObjectModelDemo/PageObjectModelDemo/Pages/BasePage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace PageObjectModelDemo.Pages
+{
+    public  class BasePage
+    {
+        protected IWebDriver Driver;
+
+        public BasePage(IWebDriver driver)
+        {
+            Driver=driver;
+        }
+        protected IWebElement FindElement(By by)
+        {
+            return Driver.FindElement(by);
+        }
+        protected IReadOnlyCollection<IWebElement> FindElements(By by)
+        {
+            return Driver.FindElements(by);
+        }
+    }
+}
---
+++ labs/PageObjectModelDemo/PageObjectModelDemo/Pages/ProductPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjectModelDemo.Pages
+{
+    internal class ProductPage
+    {
+    }
+}
