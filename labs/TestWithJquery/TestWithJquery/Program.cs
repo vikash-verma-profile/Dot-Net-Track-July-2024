61f35ea62fb0da688643bda31c4930ad4f2b5310
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-09-05 19:05:00 +0530

    added code

---
+++ labs/TestWithJquery/TestWithJquery/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace TestWithJquery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demoqa.com/automation-practice-form");
+            WebDriverWait wait = new WebDriverWait(driver,TimeSpan.FromSeconds(10));
+            //wait.Until(d => d.FindElement(By.Name("q")));
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+
+            //js.ExecuteScript("$(\"input[name='q']\").val('jQuery');");
+            //js.ExecuteScript("$(\"input[name='q']\").submit()");
+
+            js.ExecuteScript("$('#firstName').val('jQuery');");
+            //js.ExecuteScript("$(\"input[name='q']\").submit()");
+            //driver.Quit();
+        }
+    }
+}
