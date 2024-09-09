f2175b3bf144e0a5b9088459f82bbfea9ef2c781
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-09-09 18:53:51 +0530

    added win32 control

---
+++ labs/DemoOnWin32/DemoOnWin32/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DemoOnWin32
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int MessageBox(IntPtr hWnd,string text,string caption,uint type);
+        static void Main(string[] args)
+        {
+            MessageBox(IntPtr.Zero,"I am a test asdasd","Heading some dummy text",0);
+        }
+    }
+}
