f78878f14b2a28482d89261fb611fefd41f48d85
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-10-03 19:10:38 +0530

    added code

---
+++ vssamples/sampleapp/Employee.cs
@@ -0,0 +1,4 @@
+public class Employee{
+    public int Id { get; set; }
+    public string Name {get;set;}
+}
---
+++ vssamples/sampleapp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Employee emp=new Employee();
+Console.WriteLine("Please enter employee id");
+emp.Id=Convert.ToInt32(Console.ReadLine());
+emp.Name="Vikash";
+Console.WriteLine($"employee details are : Employee Id {emp.Id} Employee Name: {emp.Name}" );
