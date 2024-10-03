14adaa260ed34aae3a1ca51d510b81aa7c3ec864
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-10-03 20:13:16 +0530

    added code

---
+++ vssamples/sampleappnuget/Program.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+var person = new Person
+{
+    Name = "Vikash Verma",
+    Age = 42,
+    Email = "vikash.verma@xyz.com"
+};
+var json = JsonConvert.SerializeObject(person);
+Console.WriteLine(json);
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Email { get; set; }
+}
